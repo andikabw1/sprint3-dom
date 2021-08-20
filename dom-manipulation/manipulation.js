368d9c0eca313880540f0942735d8cfc6932b8c3
andikabw1 <andikabaguswicaksono5@gmail.com>
2021-08-20 21:53:03 +0700

    commit last day of java script

--- dom-manipulation/manipulation.js
+++ dom-manipulation/manipulation.js
@@ -1,26 +1,26 @@
 // Element Manipulation
-const judul = document.getElementById('judul');
-const p2 = document.getElementsByClassName('p2')[0];
+// const judul = document.getElementById('judul');
+// const p2 = document.getElementsByClassName('p2')[0];
 
-const alif = document.querySelector('#b ul li:nth-child(2)')
+// const alif = document.querySelector('#b ul li:nth-child(2)')
 // judul.innerHTML = 'dojobox.id'
 
 //element.innerHTML --> mengubah isi dari tag yang sudah dipilih.
-judul.innerHTML = 'dojobox.id'
+// judul.innerHTML = 'dojobox.id'
 
 //element.style.<property> --> mengubah syle dari elemen yang dipilih.
-p2.style.backgroundColor = 'orange'
+// p2.style.backgroundColor = 'orange'
 
 //element.setAttribute() --> menambahkan attribute pada HTML.
-alif.setAttribute('id','mentor2')
+// alif.setAttribute('id','mentor2')
 
 
 //element.classList --> memanipulasi kelas
 
 //classList.add() --> menambah kelas baru
-p2.classList.add('satu');
-p2.classList.add('dua');
-p2.classList.add('tiga');
+// p2.classList.add('satu');
+// p2.classList.add('dua');
+// p2.classList.add('tiga');
 //classList.remove() --> menghilangkan kelas yang ada
 //classList.toggle() --> jika elemen kelas tidak ada, method ini akan menambahkan. jika elemen sudah punya, maka akan menghilangkan kelas tertentu.
 //classList.contains() --> mengetahui kelas tertentu.
@@ -37,65 +37,73 @@ p2.classList.add('tiga');
 
 //untuk menambahakan paragraph baru
 //kita bikin elementya dulu
-const pBaru = document.createElement('p')
+// const pBaru = document.createElement('p')
 //bikin tulisannya
-const tulisan = document.createTextNode('ini paragrap baru')
+// const tulisan = document.createTextNode('ini paragrap baru')
 //simpan tulisan ke paragraph
 //node.appendChild() 
-pBaru.appendChild(tulisan)
+// pBaru.appendChild(tulisan)
 //cara tampilkan ke document adalah dengan menambahkan pbaru ke sectionA
-const sectionA = document.getElementById('a')
-sectionA.appendChild(pBaru)
+// const sectionA = document.getElementById('a')
+// sectionA.appendChild(pBaru)
 
 
 //bikun element penampung l1
-const newLi = document.createElement('li')
+// const newLi = document.createElement('li')
 //bkin isinya
-const isiElement = document.createTextNode('Andika')
+// const isiElement = document.createTextNode('Andika')
 //kita gabungkan isi ke element penampung
-newLi.appendChild(isiElement)
+// newLi.appendChild(isiElement)
 
 //cari wadah
-const wadahDariLI = document.querySelector('#b ul')
+// const wadahDariLI = document.querySelector('#b ul')
 
 //define lokasi => taruh sebelum li alif
-const liAlif = wadahDariLI.querySelector('li:nth-child(2)')
+// const liAlif = wadahDariLI.querySelector('li:nth-child(2)')
 
 //node.insertBefore(elementYangDitambahkan, ElementTujuan)
-wadahDariLI.insertBefore(newLi,liAlif)
+// wadahDariLI.insertBefore(newLi,liAlif)
 
 //define dulu element yang akan kita hapus
-const elementYangAkanDiHapus = document.getElementsByTagName('a')[0]
+// const elementYangAkanDiHapus = document.getElementsByTagName('a')[0]
 
 //define parentNode dari element yang akan dihapus /define node nya
 // const sectionA = document.getElementById('a')
 
 //parentNode.removeChild(elementYangAkanDihapus)
-sectionA.removeChild(elementYangAkanDiHapus)
+// sectionA.removeChild(elementYangAkanDiHapus)
 
 //buat element baru berupa h2
-const h2 = document.createElement('h2')
+// const h2 = document.createElement('h2')
 //buat node text untuk h2
-const tulisanBaru = document.createTextNode('Para Mentor')
+// const tulisanBaru = document.createTextNode('Para Mentor')
 //gabungkan node text ke h2
-h2.appendChild(tulisanBaru)
+// h2.appendChild(tulisanBaru)
 
 //define p4
-const sectionB = document.getElementById('b')
-const p4 = sectionB.querySelector('p')
+// const sectionB = document.getElementById('b')
+// const p4 = sectionB.querySelector('p')
 
 //define parentNode
 
 
 //replace p4 menjadi h2
-sectionB.replaceChild(h2,p4)
+// sectionB.replaceChild(h2,p4)
 
 
 //parentNode.replaceChild()
 
+//Node Manipulation
 
+//bikin elementnya
+const xx = document.createElement('p');
+const tulisan = document.createTextNode('paragraph baru');
+xx.appendChild(tulisan)
 
+//section A
+const sectionA = document.getElementById('a')
 
+sectionA.appendChild(xx)
 
 
 
